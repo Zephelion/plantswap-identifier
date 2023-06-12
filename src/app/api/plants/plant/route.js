af5b1420bcc27e68f63d2d6931cba7ceb6f64b1e
Ymaro <wyroneblue@gmail.com>
2023-06-12 20:22:08 +0200

    added api route and fetch functiomaloty for a plant

---
+++ src/app/api/plants/plant/route.js
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server';
+import FormData from 'form-data';
+import axios from 'axios';
+import { supabase } from '@/../lib/supabaseClient';
+
+export const runtime = 'nodejs';
+const TABLE_NAME = 'cuttings';
+
+export async function GET(req) {
+
+    const { searchParams } = new URL(req.url)
+    const id = searchParams.get('id')
+    console.log(id)
+
+    const { data, error } = await supabase
+        .from(TABLE_NAME)
+        .select('*')
+        .eq('id', id)
+        .single()
+    ;
+
+    if (error) {
+        console.log(error)
+        return NextResponse.error(error)
+    }
+
+    return NextResponse.json({ data });
+}
