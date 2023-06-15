1152100b64b43aca16421c4e59995517f449ae91
Ymaro <wyroneblue@gmail.com>
2023-06-15 14:28:12 +0200

    updated searchbar component

--- src/components/searchbar/index.jsx
+++ src/components/searchbar/index.jsx
@@ -1,15 +1,41 @@
 'use client'
 import Styles from './styles.module.scss'
+import RadioInput from '@/components/common/input/radio'
 
-const SearchBar = ({ input = '', setInput }) => {
+const SearchBar = ({ input = '', setInput, setIsTaken }) => {
     return (
-        <input
-            className={Styles.search}
-            type="search"
-            placeholder="Zoek een plant..."
-            value={input}
-            onChange={e => setInput(e.target.value)}
-        />
+        <form className={Styles.search}>
+            <section >
+                <span>🔍</span>
+                <input
+                    type="search"
+                    placeholder="Zoek een plant..."
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
+                />
+                {
+                    input && <button onClick={e => setInput('')}>
+                        ✖
+                    </button>
+                }
+            </section>
+            <fieldset>
+                <RadioInput 
+                    id="is-taken-false"
+                    name="is-taken"
+                    label="Beschikbaar"
+                    value={false}
+                    updateChange={() => setIsTaken(false)}
+                />
+                <RadioInput
+                    id="is-taken-true"
+                    name="is-taken"
+                    label="Niet beschikbaar"
+                    value={true}
+                    updateChange={() => setIsTaken(true)}
+                />
+            </fieldset>
+        </form>
     )
 }
 
