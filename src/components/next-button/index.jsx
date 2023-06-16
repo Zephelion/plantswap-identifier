52d51b8376df78f03c665edd9aabbd86787a32df
Ymaro <wyroneblue@gmail.com>
2023-06-17 00:18:16 +0200

    updated next component

--- src/components/next-button/index.jsx
+++ src/components/next-button/index.jsx
@@ -2,7 +2,7 @@ import Styles from "./styles.module.scss";
 
 const NextButton = ({label = "Doorgaan", clickFunction}) => {
   return (
-    <button className={Styles.next} onClick={() => clickFunction(3)}>{label}</button>
+    <button className={Styles.next} onClick={clickFunction}>{label}</button>
   )
 }
 
