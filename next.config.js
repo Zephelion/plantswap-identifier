29b7ef4a24897e137b8836aecf785692ea93de8f
Ymaro <wyroneblue@gmail.com>
2023-06-17 00:16:38 +0200

    Update next.config.js
    added plantnet as image source

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require('path')
 const nextConfig = {
-    // sassOptions: {
-    //     includePaths: [path.join(__dirname, 'styles')],
-    //   },
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'bs.plantnet.org',
+            }
+        ],
+    },
 }
 
 module.exports = nextConfig
