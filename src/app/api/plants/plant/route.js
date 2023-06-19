5856df1cd3ab5fcd810fd537fd39be3af1b08a12
Ymaro <wyroneblue@gmail.com>
2023-06-19 23:24:03 +0200

    added ne plant route

--- src/app/api/plants/plant/route.js
+++ src/app/api/plants/plant/route.js
@@ -24,3 +24,33 @@ export async function GET(req) {
 
     return NextResponse.json({ data });
 }
+
+export async function POST(req) {
+
+    const body = await req.json()
+
+    const { data, error } = await supabase
+        .from(TABLE_NAME)
+        .insert({
+            name: body.form_details.name || "",
+            latin_name: body.form_details.latin_name || "",
+            origin: body.form_details.origin || "",
+            poisonous: body.form_details.poisonous || "",
+            description: body.form_details.description || "",
+            temperature: body.form_tips.temperature || "",
+            sunlight: body.form_tips.sunlight || "",
+            hydration_guide: body.form_tips.hydration_guide || "",
+        })
+        .select('id')
+        .single()
+    ;
+
+    if (error) {
+        console.log(error)
+        return NextResponse.error(error)
+    }
+
+    return NextResponse.json({ 
+        data
+    });
+}
