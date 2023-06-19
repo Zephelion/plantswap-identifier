282ed0b88400eef515394a107cb8bcec13f34230
Zephelion <russellnumo@outlook.com>
2023-06-19 18:08:47 +0200

    get all available plants and render

--- src/components/form/swap/index.jsx
+++ src/components/form/swap/index.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 import styles from "./styles.module.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { LoadingSpinner } from "@/components/loading/spinner";
+import Plant from "@/components/plant-card-available";
+import SwapComponent from "@/components/swap";
 
 export const Swap = ({
     formDetails,
@@ -7,17 +12,51 @@ export const Swap = ({
     image,
 }) => {
 
+    const [plants, setPlants] = useState([]);
+    const [chosenPlant, setChosenPlant] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchPlants = async (search = '', isTaken = 'false') => {
+        const data = await axios.get('/api/plants', {
+            params: {
+                search,
+                is_taken: isTaken,
+            }
+        })
+        return data.data
+    }
+
+    useEffect(() => {
+        (async () => {
+            setIsLoading(true)
+            const { data } = await fetchPlants()
+            setPlants(data)
+            setIsLoading(false)
+        })()
+    }, []);
+
+    console.log(chosenPlant)
 
     
 	return (
-        <section>
-            <pre>
-                {JSON.stringify(formDetails, null, 2)}
-            </pre>
-            <pre>
-                {JSON.stringify(formTips, null, 2)}
-            </pre>
-            <Image src={image.src} alt="Identified plant" width={200} height={200} />
-        </section>
+        <>
+            <div>
+                <div className={styles.heading}>
+                    <div className={styles.title}>
+                        <h1>Doneren/ruilen</h1>{plants.length > 1 ? <span>({plants.length} planten beschikbaar)</span> : <span>({plants.length} plant beschikbaar)</span>}
+                    </div>
+                    <p>Selecteer een plant waarmee u wilt ruilen</p>
+                </div>
+                {isLoading ? <LoadingSpinner /> : <ul className={styles.container}>
+                    {plants.map(plant => (
+                        <Plant key={plant.id} name={plant.name} image={plant.image} id={plant.id} chosenPlant={setChosenPlant} />
+                    ))}
+                    </ul>
+                }
+            </div>
+
+            <SwapComponent chosenPlant={chosenPlant} name={formDetails} image={image}/>
+        </>
+
 	)
 }
