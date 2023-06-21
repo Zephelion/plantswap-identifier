a7b01db9fa096c0ba0a6f318a22336146193f481
Ymaro <wyroneblue@gmail.com>
2023-06-22 00:40:33 +0200

    Update index.js
    header images fix

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,6 +3,7 @@
 import { usePathname } from 'next/navigation';
 import styles from './styles.module.scss';
 import Link from "next/link";
+import Image from 'next/image';
 
 
 
@@ -14,19 +15,19 @@ export default function Footer() {
         <ul>
           <li className={route === '/' ? styles.active : ''}>
             <Link href="/">
-              <img src="/icons/home.svg"></img>
+              <Image src="/icons/home.svg" width={30} height={30} alt="Afbeelding voor home link"/>
               <p>Home</p>
             </Link>
           </li>
           <li className={route === '/plants' ? styles.active : ''}>
             <Link href="/plants">
-              <img src="/icons/plants.svg"></img>
+              <Image src="/icons/plants.svg" width={30} height={30} alt="Afbeelding voor planten link"/>
               <p>Planten</p>
             </Link>
           </li>
           <li className={route === '/plants/new' ? styles.active : ''}>
             <Link href="/plants/new">
-              <img src="/icons/add.svg"></img>
+              <Image src="/icons/add.svg" width={30} height={30} alt="Afbeelding voor ruilen en doneren link"/>
               <p>Doneer/ruil</p>
             </Link>
           </li>
