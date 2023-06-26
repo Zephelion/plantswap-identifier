2344d0e8dfba7fbd59e6cf48e83c8a0ababada27
Ymaro <wyroneblue@gmail.com>
2023-06-26 12:38:32 +0200

    Pwa manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,12 @@
 import BaseLayout from '@/components/layouts/base-layout';
 import '@/styles/app.scss';
 
+export const metadata = {
+    title: "Plantswap",
+    description: "Doneer en ruil planten met je buren",
+    manifest: "/manifest.json",
+}
+
 export default function RootLayout({ children }) {
     
     return (
